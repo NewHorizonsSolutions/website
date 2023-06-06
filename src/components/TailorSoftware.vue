b4a691d86b46f3b0e2aa29f108a4eb9a67a52baf
Valentino Indovino <valetero30@gmail.com>
2023-06-06 02:07:12 -0300

    Revert "Update TailorSoftware.vue"
    This reverts commit 4e61d56b625b5952caf344a2a362012bd13ee4be.

--- src/components/TailorSoftware.vue
+++ src/components/TailorSoftware.vue
@@ -24,7 +24,7 @@
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 120"
         preserveAspectRatio="none"
-        style="transform: rotate(180deg); position: absolute; bottom: 0"
+        style="transform: rotate(180deg); position: absolute; bottom: 0;"
       >
         <path
           d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
@@ -59,7 +59,6 @@
   color: black;
   padding: 10rem 0 0;
   background-color: #e2e1e1;
-  position: relative;
 }
 
 .monitor {
@@ -118,8 +117,8 @@
   background: -webkit-linear-gradient(to right, #232526, #414345); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(to bottom, #232526, #414345);
   color: white;
-  padding: 9rem 0 17rem;
-  margin: 200px 0px 0px;
+  padding: 5rem 0 13rem;
+  margin: 90px 0px 0px;
   position: relative;
 }
 
@@ -128,7 +127,7 @@
     width: 500px !important;
   }
 
-  .budget {
+  .budget{
     padding: 5rem 0 7rem;
   }
 }
