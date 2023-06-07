36af8e48111b061b8a1f929f0ba97da1cd98d1eb
Valentino Indovino <valetero30@gmail.com>
2023-06-07 15:02:12 -0300

    Update CustomersComponent.vue

--- src/components/CustomersComponent.vue
+++ src/components/CustomersComponent.vue
@@ -6,17 +6,17 @@
       <v-row no-gutters style="overflow: hidden; justify-content: center">
         <v-col order="6" class="m-3">
           <v-sheet class="pa-2 ma-2" style="background: transparent; text-align: center">
-            <img src="nouvelle.png" alt="" />
+            <span> <img src="nouvelle.png" alt="" /> </span>
           </v-sheet>
         </v-col>
         <v-col order="12" class="m-3">
           <v-sheet class="pa-2 ma-2" style="background: transparent; text-align: center">
-            <img src="lwd.png" alt="" style="padding-top: 20px" />
+            <span> <img src="lwd.png" alt="" style="padding-top: 20px" /> </span>
           </v-sheet>
         </v-col>
         <v-col order="1" class="m-3">
           <v-sheet class="pa-2 ma-2" style="background: transparent; text-align: center">
-            <img src="mcpapers.png" alt="" style="padding-top: 20px" />
+            <span> <img src="mcpapers.png" alt="" style="padding-top: 20px" /> </span>
           </v-sheet>
         </v-col>
       </v-row>
@@ -32,7 +32,8 @@
   justify-content: center;
   background: #232526;
   background: -webkit-linear-gradient(to right, #232526, #414345);
-  background: linear-gradient(to bottom, #232526, #414345);  color: white;
+  background: linear-gradient(to bottom, #232526, #414345);
+  color: white;
   padding: 6rem 0;
   position: relative;
 }
@@ -43,7 +44,7 @@ img {
 }
 
 @media screen and (max-width: 575px) {
-  img{
+  img {
     /* width: -webkit-fill-available; */
   }
 }
